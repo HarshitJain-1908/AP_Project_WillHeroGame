9314fd9dae6e70f2118da75a11e5e751539ef400
HarshitJain-1908 <harshit20063@iiitd.ac.in>
2021-12-05 19:50:27 +0530

    Load and Home page added.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,45 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class Controller {
+
+    public void clickOnPlay(ActionEvent e) {
+        System.out.println("Let's play");
+    }
+
+    //@FXML
+
+    //ImageView myImageView;
+    @FXML
+    Button LoadButton, play;
+    //Image image = new Image(getClass().getResourceAsStream("HomePage.jpeg"));
+
+    public void clickOnLoad(ActionEvent e) throws IOException {
+        switchToScene2(e);
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void playGame(ActionEvent e){
+        System.out.println("play game");
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,16 +13,19 @@ import java.awt.*;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception{
-
-        stage.setTitle("Will Hero");
-        Group root= new Group();
-        Scene scene= new Scene(root);
-        stage.setScene(scene);
-        Image icon=new Image("Icon.jpeg");
-        stage.getIcons().add(icon);
-        stage.show();
-
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            stage.setTitle("Will Hero");
+            //Group root= new Group();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            Image icon = new Image("Icon.jpeg");
+            stage.getIcons().add(icon);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
